22a5ddf09e105e4018dd477a7be1953dc62d9639
Jonas Lindberg <cj.jonas@gmail.com>
2022-10-19 17:16:06 +0200

    Working prototype

--- src/main.py
+++ src/main.py
@@ -8,16 +8,14 @@ print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
 class SpeedObj():
-	def __init__():
-		self.st = start_time
-		self.et = end_time
+	def __init__(self):
 		self.performance_down = []
 		self.performance_up = []
 		self.timemark = []
 
 	def update(self, results_dict):
-		self.performance_down.append(results_dict)
-		self.performance_up.append(results_dict)
+		self.performance_down.append(results_dict["download"])
+		self.performance_up.append(results_dict["upload"])
 		self.timemark.append(datetime.datetime.now)
 
 	def retrieve(self):
@@ -37,14 +35,14 @@ def perform_check():
 	# If you want to use a single threaded test
 	# threads = 1
 
-	s = speedtest.Speedtest()
+	s = speedtest.Speedtest(secure=True)
 	s.get_servers(servers)
 	s.get_best_server()
 	s.download(threads=threads)
 	s.upload(threads=threads)
 	s.results.share()
 
-	results_dict = s.results.dict()
+	return s.results.dict()
 
 
 def arg_parser():
@@ -73,9 +71,17 @@ def main():
 	end_time = start_time + datetime.timedelta(hours=args.duration)
 
 	while datetime.datetime.now() < end_time:
-		perform_check()
+		result_dict = perform_check()
+		speed.update(result_dict)
 		time.sleep(args.interval * 60)
 
+	down, up, _ = speed.retrieve()
+	print("Download speeds:")
+	print(down)
+	print("Upload speeds:")
+	print(up)
 
 if __name__ == "__main__":
+
+	# https://stackoverflow.com/questions/56326644/python-speedtest-facing-problems-with-certification-ssl-c1056
 	main()
